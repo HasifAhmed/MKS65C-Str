d857ef36e6a2602bcd0b9c11d2913faed89bfe3c
hasifahmed <hahmed@stuy.edu>
2018-09-28 15:33:50 -0400

    finished mystr

--- myStr.c
+++ myStr.c
@@ -8,13 +8,37 @@ int myStrlen(char * src){
   return x;
 }
 
+char* myStrcpy( char *dest, char *source ){
+  
+  while(*dest++  = *source++);
+  return dest;
+  
+}
+
+
+
 int main(){
   char * lenTest = "Hello";
-  printf("Length of 'Hello': %d", myStrlen(lenTest)); 
+  printf("Length of 'Hello': %d\n", myStrlen(lenTest)); 
+  printf("Length of 'Hello': %ld\n", strlen(lenTest));
 
+  printf("\n\n\n--------------------");
+  
+  char* ScpyTest = "Hello";
+  char DcpyTest[] = "Goodbye";
+  printf("\nDestination holds: %s\n",DcpyTest);
+  printf("Source holds: %s\n", ScpyTest);
+  myStrcpy(DcpyTest,ScpyTest);
+  printf("Destination now holds: %s\n", DcpyTest);
 
+  char* ScpyTest2 = "Hello";
+  char  DcpyTest2[] = "Goodbye";
+  printf("\nDestination holds: %s\n",DcpyTest2);
+  printf("Source holds: %s\n", ScpyTest2);
+  strcpy(DcpyTest2,ScpyTest2);
+  printf("Destination now holds: %s\n", DcpyTest2);
 
-
+  
   return 0;
 
 
